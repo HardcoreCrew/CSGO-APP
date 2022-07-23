c4f4dc0d34001f55b663a3eae70dd302fd3b5e7d
Vorczu <vorczu89@gmail.com>
2022-07-23 14:21:41 +0200

    BASE FRON UPDATE
    + GLOBAL STYLE
    + THEME PROVIDER
    + LANG CONTEXT
    + BASE THEMES (Dark and Light)
    + Styled components

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client'
-import './App.css';
+import {langPl, langEn} from './utils/langs.js'
+import {LangContext} from './context/langContext.js'
+import {ThemeProvider} from 'styled-components'
+import { lightTheme, darkTheme} from './styles/themes.js'
+import GlobalCSS from './styles/global.styled.js'
 
 const socket = io.connect("http://localhost:3137")
 
 function App() {
+
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [lastPong, setLastPong] = useState(null);
   const [msgToSend, setMsgToSend] = useState();
   const [messages, setMessages] = useState([]);
+  const [themeState, setThemeState] = useState('dark')
+  const [languageState, setLanguageState] = useState('pl')
+  
+  const handleTheme = () =>{
+    themeState === 'light' ? setThemeState('dark') : setThemeState('light')
+  }
+  
+  const handleLanguage = () =>{
+    languageState === 'pl' ? setLanguageState('en') : setLanguageState('pl')
+  }
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -45,11 +60,17 @@ function App() {
 
   
   return ( 
-    <div className="App">
-      <input placeholder='tekst...' onChange={e => setMsgToSend(e.target.value)} />
-      <button onClick={sendMessage} >SEND</button><br/>
-      {messages? messages.map(el => <div>{el.id} - {el.msg} </div>) : 'DUPA'}
-    </div>
+    <LangContext.Provider value={languageState ==='pl'? langPl : langEn}>
+    <ThemeProvider theme={themeState ==='light'? lightTheme : darkTheme}>
+    <GlobalCSS />
+      <div className="App">
+        <label >SEND MSG: </label>
+        <input type="text" />
+        <button>SEND</button>
+      </div>
+    </ThemeProvider> 
+
+    </LangContext.Provider>
   );
 }
 
---
+++ front_end/src/styles/themes.js
@@ -0,0 +1,13 @@
+
+export const darkTheme = {
+    body: '#111',
+    color: '#FFFFFF',
+    _colorFont: '#127edf',
+    _lightBox: '#05131f',
+    _lightBoxText: '#1c2d3d'
+}
+
+export const lightTheme = {
+    body: '#fff',
+    color: '#333'
+}
