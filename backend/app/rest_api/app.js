80f8dbed0ab73e36f7e8f03eb75b4641ed64565e
jacek grzywniak <jacek.grzywniak@atos.net>
2022-07-26 00:43:16 +0200

    created swagger docs for addServerRequest endpoint, refactored CsGoServersCrud controller and createApp function

--- backend/app/rest_api/app.js
+++ backend/app/rest_api/app.js
@@ -1,11 +1,25 @@
 import express from 'express'
-import * as endpoints from './endpoints/index.js'
+import dotenv from 'dotenv'
+import controllers from './endpoints/index.js'
+import swaggerUI from 'swagger-ui-express'
+import swaggerBaseDoc from './swagger_base_doc.js'
 
+
+dotenv.config()
 
 export default function createApp() {
+    const url = process.env.API_BASE_URL || 'http://localhost:3000'
+    const port = process.env.API_PORT || 3000
+
     const app = express()
     app.use(express.json())
-    app.use('/api', endpoints.v1.cSGoServersCrudRouter)
 
-    return app
+    const router = express.Router()
+    controllers.map(controller => controller.registerMethods(router))
+    app.use('/api', router)
+
+    swaggerBaseDoc.servers[0].url = `${url}/api`
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerBaseDoc))
+
+    return { app, port }
 }
---
+++ backend/app/rest_api/endpoints/v1/CsGoServersCrudDoc.js
@@ -0,0 +1,86 @@
+import v1Url from './v1_base_url.js'
+
+
+const BASE_URL = `${v1Url}/cs-go-servers`
+
+const inputContent = {
+    "application/json": {
+        schema: {
+            type: "object",
+            properties: {
+                lobby_id: {
+                    type: "integer",
+                    example: 1
+                },
+                maps: {
+                    type: "array",
+                    items: {
+                        type: "string"
+                    }
+                }
+            }
+        }
+    }
+}
+
+const props = {
+    id: {
+        type: "integer",
+        example: 1
+    },
+    ...inputContent['application/json'].schema.properties,
+    state: {
+        type: "string",
+        enum: ["IN_PROGRESS", "DONE"]
+    },
+    status: {
+        type: "string",
+        enum: ["UNDETERMINED", "SUCCESS", "FAILURE"]
+    }
+}
+
+const outputContent = {
+    "application/json": {
+        schema: {
+            type: "object",
+            properties: props
+        }
+    }
+}
+
+const doc = {
+    [BASE_URL]: {
+        post: {
+            tags: ["CS GO server requests"],
+            description: "CS GO server requests CRUD operations",
+            operationId: "addServerRequest",
+            requestBody: {content: inputContent},
+            responses: {
+                202: {
+                    description: "CS GO server request accepted",
+                    content: outputContent
+                },
+                400: {
+                    description: "Invalid request",
+                    content: {
+                        "application/json": {
+                            schema: {
+                                type: "object",
+                                properties: {
+                                    Error: {
+                                        type: "string"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+export {
+    doc,
+    BASE_URL,
+}
