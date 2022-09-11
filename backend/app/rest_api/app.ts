18b996c31e87efa6709f514b101538fae65c3067
jacek grzywniak <jacek.grzywniak@atos.net>
2022-09-11 11:20:05 +0200

    removed commented code in app.ts

--- backend/app/rest_api/app.ts
+++ backend/app/rest_api/app.ts
@@ -1,29 +1,9 @@
 import * as express from 'express'
 import * as swaggerUI from 'swagger-ui-express'
 import * as cors from 'cors'
-import { 
-    Request, 
-    Response, 
-    NextFunction, 
-} from 'express'
 import controllers from './endpoints'
 import swaggerBaseDoc from './swagger_base_doc'
 import { DotEnvSettings } from '../settings_implementation'
-
-
-// dotenv.config()
-
-
-// function expressUnitOfWork(req: Request, res: Response, next: NextFunction) {
-//     console.log('REQUEST STARTED');
-//     res.on('finish', async () => {
-//         console.log('REQUEST ENDED');
-//         // You might have to add a logic here like if(req.user) then perform following things
-//         // Do whatever you want to do here after every API call finishes
-//         // you can access res.statusCode, req.method etc.
-//         });
-//         next();
-// }
 
 
 export default function createApp() {
@@ -34,7 +14,6 @@ export default function createApp() {
     const app = express()
     app.use(express.json())
     app.use(cors())
-    // app.use(expressUnitOfWork)
 
     const router = express.Router()
     controllers.map(controller => controller.registerMethods(router))
