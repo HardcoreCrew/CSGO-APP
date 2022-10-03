f8d2da868c8b7484b58d5c456a51312d8e6a45d2
Vorczu <vorczu89@gmail.com>
2022-10-03 08:41:56 +0200

    update

--- ws_chat/server.js
+++ ws_chat/server.js
@@ -1,56 +1,71 @@
-const expres = require('express');
-const http = require('http')
-const socketio = require('socket.io')
-const cors = require('cors')
+// const expres = require('express');
+// const http = require('http')
+// const socketio = require('socket.io')
+// const cors = require('cors')
 
-const app = expres();
-app.use(cors())
-const server = http.createServer(app, {
-    cors
-})
-const io = socketio(server)
+// const app = expres();
+// app.use(cors())
+// const server = http.createServer(app, {
+//     cors
+// })
+// const io = socketio(server)
 
-let onlinePlayers = []
+// let onlinePlayers = []
 
 
-io.on('connection', socket => {
-    // io.to(socket.id).emit('my', onlinePlayers)
-    console.log(`Połączenie: ${socket.id}`);
+// io.on('connection', socket => {
+//     // io.to(socket.id).emit('my', onlinePlayers)
+//     console.log(`Połączenie: ${socket.id}`);
 
-    setInterval(() => {
-        io.emit('ping')        
-    }, 60000);
+//     setInterval(() => {
+//         io.emit('ping')        
+//     }, 60000);
     
-    socket.on('isOn', (data) =>{
-        onlinePlayers.push({id: socket.id, userData: data})
-        console.log(onlinePlayers);
-        io.emit('onlinedata', onlinePlayers)
-    })
+//     socket.on('isOn', (data) =>{
+//         onlinePlayers.push({id: socket.id, userData: data})
+//         console.log(onlinePlayers);
+//         io.emit('onlinedata', onlinePlayers)
+//     })
 
-    socket.on('isOff', (data) =>{
-        console.log('UCIEKA: ' + socket.id);
-        onlinePlayers = onlinePlayers.filter(x => x.id != socket.id)
-        io.emit('onlinedata', onlinePlayers)
-    })
+//     socket.on('isOff', (data) =>{
+//         console.log('UCIEKA: ' + socket.id);
+//         onlinePlayers = onlinePlayers.filter(x => x.id != socket.id)
+//         io.emit('onlinedata', onlinePlayers)
+//     })
 
-    socket.on('message', (data) =>{
-        socket.broadcast.emit('new_message', {...data, self: false})
-        console.log(data);
-    })
+//     socket.on('message', (data) =>{
+//         socket.broadcast.emit('new_message', {...data, self: false})
+//         console.log(data);
+//     })
 
-    socket.on('joinRoom', (data) =>{
-        socket.broadcast.emit('new_message', {...data, self: false})
-        console.log(data);
-    })
+//     socket.on('joinRoom', (data) =>{
+//         socket.broadcast.emit('new_message', {...data, self: false})
+//         console.log(data);
+//     })
 
-    socket.on('pong', (data) =>{
-        console.log(socket.id);
+//     socket.on('pong', (data) =>{
+//         console.log(socket.id);
         
-    })
+//     })
 
     
-})
+// })
 
 
 
-server.listen(3137, ()=> console.log('Lecimy!'))
+// server.listen(3137, ()=> console.log('Lecimy!'))
+
+
+
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	channels: [ 'spite' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+});
